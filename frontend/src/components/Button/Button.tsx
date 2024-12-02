9722a865fcee9de0b796fde09769bde7f0df07ce
amritchapai <chapaiamrit@gmail.com>
2024-12-03 02:03:10 +0545

    completed navbar

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -4,12 +4,11 @@ import "./button.css"
 interface buttonProps {
   text: string;
   handler: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  className?: string;
 }
 const Button: React.FC<buttonProps> = (props) => {
   return (
     <button
-      className={`button ${props.className}`}
+      className="button"
       onClick={(e) => props.handler(e)} 
     >
       {props.text}
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./navbar.css"
+import Button from "../Button/Button";
 
 const Navbar: React.FC = () => {
 
+  const logoutHandler = ()=>{}
   return (
     <div>
       <div className="navbar-container">
         <strong>ToDO WebApp</strong>
+        <div>
+          <Button text="Logout" handler={logoutHandler} />
+        </div>
       </div>
     </div>
   );
