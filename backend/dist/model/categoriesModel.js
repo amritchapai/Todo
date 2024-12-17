8a6270d93b46ec3afd93ae5dafb569a82d0a9b5a
amritchapai <chapaiamrit@gmail.com>
2024-12-17 15:17:36 +0545

    resolved

--- backend/dist/model/categoriesModel.js
+++ backend/dist/model/categoriesModel.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+//make schema
 const categorySchema = new mongoose_1.default.Schema({
     owner: { type: mongoose_1.default.Schema.Types.ObjectId, required: true, ref: "User" },
     categoryName: { type: String, required: true, unique: true },
     tasks: [{ type: mongoose_1.default.Schema.Types.ObjectId, ref: "Task" }]
 }, { timestamps: true });
+//model
 const Category = mongoose_1.default.model("Category", categorySchema);
 exports.default = Category;
--- backend/src/model/categoriesModel.ts
+++ backend/src/model/categoriesModel.ts
@@ -6,13 +6,14 @@ export interface ICategory extends mongoose.Document{
     tasks: mongoose.Types.ObjectId[],
 }
 
+//make schema
 const categorySchema = new mongoose.Schema<ICategory>({
     owner: {type: mongoose.Schema.Types.ObjectId, required:true, ref:"User"},
     categoryName: {type: String, required: true, unique: true},
     tasks: [{type: mongoose.Schema.Types.ObjectId, ref: "Task"}]
 }, {timestamps: true});
 
-
+//model
 const Category = mongoose.model<ICategory>("Category", categorySchema);
 
 export default Category;
