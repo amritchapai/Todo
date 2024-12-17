606ae95480c8d8d36b6fed5aa5a7b058a8e85f20
amritchapai <chapaiamrit@gmail.com>
2024-12-17 15:13:31 +0545

    no idea

--- backend/dist/routes/taskRoutes.js
+++ backend/dist/routes/taskRoutes.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authentication_1 = __importDefault(require("../middleware/authentication"));
-const taskController_1 = require("../controller/taskController.js");
+const taskController_1 = require("../controller/taskController");
 const router = express_1.default.Router();
 router.post("/addtask/:categoryId", authentication_1.default, taskController_1.addTask);
 router.post("/edittask/:taskid", authentication_1.default, taskController_1.editTask);
